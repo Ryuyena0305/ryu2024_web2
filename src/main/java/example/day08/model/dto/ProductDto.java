c3baca5a0860d0175d1d9cbb216f15ef86e9c81f
Ryuyena0305 <Ryuyena0305@gmail.com>
2025-03-11 17:31:10 +0900

    3/11 CRUD java코드

---
+++ src/main/java/example/day08/model/dto/ProductDto.java
@@ -0,0 +1,16 @@
+package example.day08.model.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDto {
+    private int id;
+    private String name;
+    private int price;
+    private String comment;
+}
---
+++ src/main/java/example/day08/service/ProductService.java
@@ -0,0 +1,42 @@
+package example.day08.service;
+
+import example.day08.model.dto.ProductDto;
+import example.day08.model.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductMapper productMapper;
+
+
+    public List<ProductDto> onRead(){
+        return productMapper.onRead();
+    }
+
+
+    public int onCreate(ProductDto productDto){
+        System.out.println("ProductService.onSave");
+        System.out.println("productDto = " + productDto);
+        return productMapper.onCreate(productDto);
+    }
+
+
+    public int onUpdate(ProductDto productDto){
+        System.out.println("ProductController.onUpdate");
+        return productMapper.onUpdate(productDto);
+    }
+
+    public int onDelete( int id){
+        System.out.println("ProductController.onDelete");
+        return productMapper.onDelete(id);
+    }
+
+}
