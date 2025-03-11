9f1060cba6254b23a181b7e46f14b03e93d38916
Ryuyena0305 <Ryuyena0305@gmail.com>
2025-03-11 17:30:04 +0900

    3/11 CRUD

--- src/main/reactapp/src/example/7_AXIOS2/Delete.jsx
+++ src/main/reactapp/src/example/7_AXIOS2/Delete.jsx
@@ -1,5 +1,23 @@
+import { useEffect,useState } from "react"
+import axios from "axios"
+import {  useNavigate } from 'react-router-dom';
+
 export default function Delete(props){
+    
+    const [id,setId] =useState('');
+    const  navigate  = useNavigate();
+    const onDelete = async () => {
+        const response = await axios.delete(`http://192.168.40.32:8080/day08/products?id=${id}`);
+
+
+        if( response.data == true ){ alert('제품삭제 성공'); navigate('/read'); } 
+        else{ alert('제품삭제실패'); }
+    } // f end 
     return (<>
-    <div>Delete 페이지</div>
+    <div>
+        <h3>Delete 페이지</h3>
+        삭제할제품번호 : <input type="number" value={id} onChange={(e)=>setId(e.target.value)}/><br/>
+        <button type="button" onClick={onDelete}>삭제</button>
+        </div>
     </>)
 }
--- src/main/reactapp/src/example/7_AXIOS2/Read.jsx
+++ src/main/reactapp/src/example/7_AXIOS2/Read.jsx
@@ -1,17 +1,46 @@
-import { useEffect,useState } from "react"
+import { useEffect, useState } from "react"
 import axios from "axios"
-export default function Read(props){
+import './Read.css'
+export default function Read(props) {
     //[1] 컴포넌트 최초 실행될 때  실행하는 생명주기 함수
-    useEffect(()=>{
+    useEffect(() => {
         console.log('여기는 컴포넌트 실행될 때 1번만 호출됩니다.')
         onRead();
-    },[])
+    }, [])
 
-    const onRead = async () =>{
-        const response = await axios.get('http://localhost:8080/day08/products')
+    const onRead = async () => {
+        const response = await axios.get('http://192.168.40.32:8080/day08/products')
         console.log(response.data);
+        setProducts(response.data); // 서버로부터 받은 모든 제품정보를 상태변수에 저장한다.
     }
+
+    //[3] 서버로부터 받은 결과를 저장하는 상태 변수
+    const [products, setProducts] = useState([])
+
     return (<>
-    <div>Read 페이지</div>
+        <div>
+            <h3>Read 페이지</h3>
+            <table>
+                <thead>
+                    <tr><th>번호</th><th>제품명</th><th>제품설명</th><th>제품가격</th></tr>
+                </thead>
+                <tbody>
+                    {
+                        products.map((product, index) => {
+                            return (
+                                <tr>
+                                    <td>{product.id}</td>
+                                    <td>{product.name}</td>
+                                    <td>{product.comment}</td>
+                                    <td>{product.price}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+
+        </div>
+
     </>)
 }
